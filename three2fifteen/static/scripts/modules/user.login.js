d117283ffeccec43bcf2ae6b1c151e1ef5cf1cae
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-06 14:09:10 +0000

    basic login form added

--- three2fifteen/static/scripts/modules/user.login.js
+++ three2fifteen/static/scripts/modules/user.login.js
@@ -1,10 +1,28 @@
-loader.addModule('page', () => {
+loader.addModule('page', 'request', 'config', 'B', (request, config, B) => {
+	function _loginResponseHandler(statusCode, statusReason, body) {
+		if (statusCode != 200) {
+			B.$id("form-message").innerHTML = JSON.parse(body).message;
+		}
+	}
+
 	return {
 		'needsLoggedIn': () => {
 			return false;
 		},
 		'action': () => {
-			console.log("login");
+			let form = B.$id('login-form');
+			form.addEventListener('submit', (e) => {
+				B.$id("form-message").innerHTML = "";
+				e.preventDefault();
+				request.post(
+					config.api_host + config.api_login,
+					JSON.stringify({
+						'username': form.username.value,
+						'password': form.password.value
+					}),
+					_loginResponseHandler
+				);
+			});
 		}
 	};
 });
