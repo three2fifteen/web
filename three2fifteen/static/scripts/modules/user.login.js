40f2e20f0b94ba950fd4b73f8fa06fcffe69627f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-08 19:14:23 +0000

    the status reason is not provided anymore

--- three2fifteen/static/scripts/modules/user.login.js
+++ three2fifteen/static/scripts/modules/user.login.js
@@ -1,7 +1,7 @@
 loader.addModule('page',
 'auth', 'request', 'config', 'B',
 (auth, request, config, B) => {
-	function _loginResponseHandler(statusCode, statusReason, body, dest_url) {
+	function _loginResponseHandler(statusCode, body, dest_url) {
 		if (statusCode == 200) {
 			auth.login(JSON.parse(body).access_token);
 			_goToUrl(dest_url);
