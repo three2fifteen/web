1204ef113477b972bf684750ed8c27f0f18effae
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-15 15:58:15 +0000

    load the games of the player in the home page

--- three2fifteen/controller/config.py
+++ three2fifteen/controller/config.py
@@ -6,5 +6,6 @@ class ConfigController(object):
         return {
             'api_host': self.config['API_HOST'],
             'api_login': self.config['API_LOGIN'],
-            'api_signup': self.config['API_SIGNUP']
+            'api_signup': self.config['API_SIGNUP'],
+            'api_get_games': self.config['API_GET_GAMES']
         }
