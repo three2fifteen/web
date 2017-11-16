a6d413ab51fbdd8053a7ed3b6ab3875035193c33
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-16 17:33:27 +0000

    Game creation form plugged

--- three2fifteen/static/scripts/modules/game.create.js
+++ three2fifteen/static/scripts/modules/game.create.js
@@ -1,7 +1,23 @@
 loader.executeModule('gameCreateModule',
-() => {
+'auth', 'request', 'config', 'B', 'utils', 'app',
+(auth, request, config, B, utils, app) => {
 	app.addModule({
 		'action': () => {
+			let form = B.$id('game-creation-form');
+			form.addEventListener('submit', (e) => {
+				B.$id("form-message").innerHTML = "";
+				e.preventDefault();
+				request.post(
+					config.api_host + config.api_create_game,
+					JSON.stringify({
+						'number_players': form.nbPlayers.value
+					}),
+					{'X-Token': auth.getToken()},
+					(statusCode, body) => {
+						utils.apiResponseHandler(statusCode, body, '/');
+					}
+				);
+			});
 		}
 	});
 });
