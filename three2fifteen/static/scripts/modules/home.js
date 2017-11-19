b6d0d0bae724f28ce525089fba49383f01609386
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-19 14:54:54 +0000

    list of games added in the page

--- three2fifteen/static/scripts/modules/home.js
+++ three2fifteen/static/scripts/modules/home.js
@@ -1,11 +1,24 @@
 loader.executeModule('homeModule',
-'config', 'app',
-(config, app) => {
+'config', 'app', 'B',
+(config, app, B) => {
 	let module = {
 		'dataUrls': [
 			{'url': config.api_get_games, 'name': 'games'}
 		],
 		'action': () => {
+			B.Template.init({
+				game: {html: B.$id('game-instance').innerHTML},
+				game_player: {html: B.$id('game-player').innerHTML}
+			});
+			module.data.games.forEach((game) => {
+				let htmlParser = new DOMParser();
+				const node = htmlParser.parseFromString(
+					B.Template.compile('game', {game: game}),
+					'text/xml'
+				);
+
+				B.$id('list-games').appendChild(node.firstChild);
+			});
 			console.log("home");
 		}
 	};
