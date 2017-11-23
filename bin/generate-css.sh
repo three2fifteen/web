9b3fdb427c73f761c6a3e16954c2f273c21bf388
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-23 12:48:07 +0000

    sass extension files changed in script

--- bin/generate-css.sh
+++ bin/generate-css.sh
@@ -2,10 +2,10 @@
 
 SASS_FOLDER=three2fifteen/sass
 CSS_FOLDER=three2fifteen/static/stylesheets
-for f in `find $SASS_FOLDER -type f -name "*.sass"`
+for f in `find $SASS_FOLDER -type f -name "*.scss"`
 do
 	echo $f
 	tmp=${f/$SASS_FOLDER/$CSS_FOLDER}
 	echo $tmp
-	./bin/sass/bin/sass $f ${tmp/.sass/.css}
+	./bin/sass/bin/sass $f ${tmp/.scss/.css}
 done
