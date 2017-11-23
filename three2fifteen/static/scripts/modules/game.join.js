3a2ddc5705876180e728bbbfcbbd8723120841c1
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-23 12:39:17 +0000

    join game implemented

--- three2fifteen/static/scripts/modules/game.join.js
+++ three2fifteen/static/scripts/modules/game.join.js
@@ -1,8 +1,21 @@
 loader.executeModule('gameJoinModule',
-'app',
-(app) => {
+'config', 'app', 'B', 'request', 'auth', 'utils',
+(config, app, B, request, auth, utils) => {
+	const handleResponse = (statusCode, body) => {
+		if (statusCode == 200) {
+			utils.goToUrl('/');
+		}
+		else {
+			B.$id('error').innerHTML = JSON.parse(body).message;
+		}
+	};
+
 	let module = {
 		'action': () => {
+			let url = config.api_host + config.api_join_game;
+			const game_public_id = B.$id('game_public_id').dataset.value;
+			url = utils.format(url, [game_public_id]);
+			request.put(url, null, auth.getHeader(), handleResponse);
 		}
 	};
 	app.addModule(module);
