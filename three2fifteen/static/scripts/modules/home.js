2cf83af32cfb3511e8e903f2bc7839b076a9e94f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-25 23:21:53 +0000

    display the link to the game if the game is started and not finished

--- three2fifteen/static/scripts/modules/home.js
+++ three2fifteen/static/scripts/modules/home.js
@@ -9,10 +9,12 @@ loader.executeModule('homeModule',
 			B.Template.init({
 				game: {html: B.$id('game-instance').innerHTML},
 				game_player: {html: B.$id('game-player').innerHTML},
+				game_link: {html: B.$id('game-link').innerHTML},
 				game_invite_link: {html: B.$id('game-invite-link').innerHTML}
 			});
 			module.data.games.forEach((game) => {
 				game.current_players_count = game.game_players.length;
+				game.ongoing = game.date_started && !game.date_finished;
 				game.open = game.current_players_count < game.number_players;
 				const gameHTML = B.Template.compile('game', {game: game});
 				B.$id('list-games').innerHTML += gameHTML;
