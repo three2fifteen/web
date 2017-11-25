ee72c3a78da24be7ecd6d247b24bc545d5888814
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-25 23:21:34 +0000

    display the invite link only if the game is open

--- three2fifteen/static/scripts/modules/home.js
+++ three2fifteen/static/scripts/modules/home.js
@@ -8,10 +8,12 @@ loader.executeModule('homeModule',
 		'action': () => {
 			B.Template.init({
 				game: {html: B.$id('game-instance').innerHTML},
-				game_player: {html: B.$id('game-player').innerHTML}
+				game_player: {html: B.$id('game-player').innerHTML},
+				game_invite_link: {html: B.$id('game-invite-link').innerHTML}
 			});
 			module.data.games.forEach((game) => {
 				game.current_players_count = game.game_players.length;
+				game.open = game.current_players_count < game.number_players;
 				const gameHTML = B.Template.compile('game', {game: game});
 				B.$id('list-games').innerHTML += gameHTML;
 			});
