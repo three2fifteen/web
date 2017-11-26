e97726d16bb226aa4f655fbef78e0ce635f3b604
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-26 17:52:32 +0000

    in the game page, load the current game

--- three2fifteen/controller/config.py
+++ three2fifteen/controller/config.py
@@ -8,6 +8,7 @@ class ConfigController(object):
             'api_login': self.config['API_LOGIN'],
             'api_signup': self.config['API_SIGNUP'],
             'api_get_games': self.config['API_GET_GAMES'],
+            'api_get_game': self.config['API_GET_GAME'],
             'api_create_game': self.config['API_CREATE_GAME'],
             'api_join_game': self.config['API_JOIN_GAME']
         }
--- three2fifteen/static/scripts/modules/game.page.js
+++ three2fifteen/static/scripts/modules/game.page.js
@@ -1,7 +1,10 @@
 loader.executeModule('gamePageModule',
-'config', 'app', 'B',
-(config, app, B) => {
+'config', 'app', 'B', 'utils', 'Game',
+(config, app, B, utils, Game) => {
 	let module = {
+		'dataUrls': [
+			{'url': utils.format(config.api_get_game, [B.$id('current_game_id').dataset.value]), 'name': 'game'}
+		],
 		'action': () => {
 		}
 	};
