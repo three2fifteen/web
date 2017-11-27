a96108bc08e526803350185026933d3b8d066894
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-27 21:23:13 +0000

    use sass from path if exists, otherwise use locally installed one

--- bin/generate-css.sh
+++ bin/generate-css.sh
@@ -2,10 +2,16 @@
 
 SASS_FOLDER=three2fifteen/sass
 CSS_FOLDER=three2fifteen/static/stylesheets
+SASS=$(which sass)
+if [ -z "$SASS" ]
+then
+	SASS=./bin/sass/bin/sass
+fi
+
 for f in `find $SASS_FOLDER -type f -name "*.scss"`
 do
 	echo $f
 	tmp=${f/$SASS_FOLDER/$CSS_FOLDER}
 	echo $tmp
-	./bin/sass/bin/sass $f ${tmp/.scss/.css}
+	$SASS $f ${tmp/.scss/.css}
 done
