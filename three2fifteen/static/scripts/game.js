283be987935a7f7c20fcc88f10a7a2b6cd92ac9d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-11-27 09:14:26 +0000

    an open game must not have a finished date
    Though in a normal process, a game should not have a finished
    date with a number of players inferior to its max number of players

--- three2fifteen/static/scripts/game.js
+++ three2fifteen/static/scripts/game.js
@@ -3,7 +3,7 @@ loader.addModule("Game", () => {
 		analyseGame: (game) => {
 			game.current_players_count = game.game_players.length;
 			game.ongoing = game.date_started && !game.date_finished;
-			game.open = game.current_players_count < game.number_players;
+			game.open = !game.date_finished && game.current_players_count < game.number_players;
 		}
 	};
 });
