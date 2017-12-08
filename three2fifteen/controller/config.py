782bee1834ab288d1469180726c53e5d2fee03a9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-08 14:04:47 +0000

    Config added for the turn endpoint

--- three2fifteen/controller/config.py
+++ three2fifteen/controller/config.py
@@ -14,5 +14,6 @@ class ConfigController(object):
             'api_get_board': self.config['API_GET_BOARD'],
             'api_get_game_content': self.config['API_GET_GAME_CONTENT'],
             'api_get_hand': self.config['API_GET_HAND'],
-            'api_turn_check': self.config['API_TURN_CHECK']
+            'api_turn_check': self.config['API_TURN_CHECK'],
+            'api_turn': self.config['API_TURN']
         }
