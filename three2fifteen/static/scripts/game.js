c43abd025751b637706027cdeed649a506b5a400
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-01-25 18:12:55 +0000

    Those are not needed anymore

--- three2fifteen/static/scripts/game.js
+++ three2fifteen/static/scripts/game.js
@@ -25,9 +25,7 @@ loader.addModule('Game',
 
 	return {
 		analyseGame: (game) => {
-			game.current_players_count = game.players.length;
 			game.ongoing = game.date_started && !game.date_finished;
-			game.open = !game.date_finished && game.current_players_count < game.number_players;
 		},
 		setPlayerScores: (game, content) => {
 			let maxScore = 0, winner = null;
