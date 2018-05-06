52a53c6d7e7c0a6a9b63a5db45c43dd538675a31
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-05-06 20:01:36 +0100

    game host added in the invite links

--- three2fifteen/static/scripts/modules/home.js
+++ three2fifteen/static/scripts/modules/home.js
@@ -18,7 +18,11 @@ loader.executeModule('homeModule',
 			Game.setPlayerNames(module.data.games).then((players) => {
 				module.data.games.forEach((game) => {
 					Game.analyseGame(game);
-					const gameHTML = B.Template.compile('game', {game: game});
+					game.host = window.location.protocol + '//' + window.location.host;
+					const gameHTML = B.Template.compile(
+						'game',
+						{game: game}
+					);
 					B.$id('list-games').innerHTML += gameHTML;
 				});
 			});
