cbaab69cf47b50b4b621c3384bfe5c4bd85cba9b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2021-02-21 11:54:38 +0100

    Take the conf from the app and not from the environment

--- three2fifteen/controller/config.py
+++ three2fifteen/controller/config.py
@@ -1,20 +1,20 @@
-import os
+from flask import current_app
 
 
 class ConfigController(object):
     def get(self):
         return {
-            'api_host': os.environ['API_HOST'],
-            'api_login': os.environ['API_LOGIN'],
-            'api_signup': os.environ['API_SIGNUP'],
-            'api_get_games': os.environ['API_GET_GAMES'],
-            'api_get_game': os.environ['API_GET_GAME'],
-            'api_create_game': os.environ['API_CREATE_GAME'],
-            'api_join_game': os.environ['API_JOIN_GAME'],
-            'api_get_board': os.environ['API_GET_BOARD'],
-            'api_get_game_content': os.environ['API_GET_GAME_CONTENT'],
-            'api_get_hand': os.environ['API_GET_HAND'],
-            'api_turn_check': os.environ['API_TURN_CHECK'],
-            'api_turn': os.environ['API_TURN'],
-            'api_get_player_names': os.environ['API_GET_PLAYER_NAMES']
+            'api_host': current_app.config['API_HOST'],
+            'api_login': current_app.config['API_LOGIN'],
+            'api_signup': current_app.config['API_SIGNUP'],
+            'api_get_games': current_app.config['API_GET_GAMES'],
+            'api_get_game': current_app.config['API_GET_GAME'],
+            'api_create_game': current_app.config['API_CREATE_GAME'],
+            'api_join_game': current_app.config['API_JOIN_GAME'],
+            'api_get_board': current_app.config['API_GET_BOARD'],
+            'api_get_game_content': current_app.config['API_GET_GAME_CONTENT'],
+            'api_get_hand': current_app.config['API_GET_HAND'],
+            'api_turn_check': current_app.config['API_TURN_CHECK'],
+            'api_turn': current_app.config['API_TURN'],
+            'api_get_player_names': current_app.config['API_GET_PLAYER_NAMES']
         }
